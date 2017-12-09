984cd90c2fc90bd0145b4d4debcc8cad87d02b94
ANIMALE\bruno.reis <brunogomesreis@gmail.com>
2017-12-09 20:25:37 -0200

    Aula 3 antes de usar $http para pegar os dados do servidor

--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -1,8 +1,20 @@
-angular.module('alurapic').controller('FotosController', function($scope) {
+angular.module('alurapic').controller('FotosController', function($scope, $http) {
     
-        $scope.foto = {
-            titulo : 'Leão',
-            url : 'http://www.fundosanimais.com/Minis/leoes.jpg'
-        };
+        $scope.fotos = [
+            {
+                titulo : 'Gibson SG',
+                url : 'http://www.gibson.com/Images/Products/Electric-Guitars/SG/Gibson-USA/Standard/Gallery-Images/SGSHCCH1-Finish-Shot3-jpg.aspx?KeepThis=true&TB_iframe=true&height=410&width=1000'
+            },
+    
+            {
+                titulo : 'Fender',
+                url : 'https://www.fmicassets.com/Damroot/ZoomJpg/10002/0112702706_gtr_frt_001_rr.jpg'
+            },
+
+            {
+                titulo : 'Gibson Less Pool',
+                url : 'http://www.gibson.com/Images/Products/Electric-Guitars/Les-Paul/Gibson-Custom/LP-Custom/LPC-AWGH1-Finish-Shot-jpg.aspx?KeepThis=true&TB_iframe=true&height=410&width=1000'
+            }
+        ];
     
     });
