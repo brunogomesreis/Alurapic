37dfc12e5c5c470d1eb078f07257dcc98d973b36
ANIMALE\bruno.reis <brunogomesreis@gmail.com>
2017-12-09 20:55:54 -0200

    Aula 3 Concluída

--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -1,20 +1,14 @@
 angular.module('alurapic').controller('FotosController', function($scope, $http) {
     
-        $scope.fotos = [
-            {
-                titulo : 'Gibson SG',
-                url : 'http://www.gibson.com/Images/Products/Electric-Guitars/SG/Gibson-USA/Standard/Gallery-Images/SGSHCCH1-Finish-Shot3-jpg.aspx?KeepThis=true&TB_iframe=true&height=410&width=1000'
-            },
+        $scope.fotos = [];
     
-            {
-                titulo : 'Fender',
-                url : 'https://www.fmicassets.com/Damroot/ZoomJpg/10002/0112702706_gtr_frt_001_rr.jpg'
-            },
-
-            {
-                titulo : 'Gibson Less Pool',
-                url : 'http://www.gibson.com/Images/Products/Electric-Guitars/Les-Paul/Gibson-Custom/LP-Custom/LPC-AWGH1-Finish-Shot-jpg.aspx?KeepThis=true&TB_iframe=true&height=410&width=1000'
-            }
-        ];
+        $http.get('/v1/fotos')
+        .success(function(retorno) {
+            console.log(retorno);
+            $scope.fotos = retorno; // não precisa fazer retorno.data
+        })
+        .error(function(erro) {
+            console.log(erro);
+        });
     
     });
